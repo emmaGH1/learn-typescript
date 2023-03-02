f8589b798a4493720c776f77e0b26a449adc1fdd
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-03-02 23:18:14 +0100

    Removed build folder from Git index

--- build/functions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function addNumbers(a, b) {
-    return a + b;
-}
-exports.default = addNumbers;
-const getName = (user) => {
-    `${user.first} ${user.last}`;
-};
